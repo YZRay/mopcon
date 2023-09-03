a28de10d09b7b7505306a9c2f7ed551082f776be
YZRay <yao19991119@gmail.com>
2023-09-03 12:29:06 +0800

    調整css

--- src/page/Unconf/UnconfCard.js
+++ src/page/Unconf/UnconfCard.js
@@ -5,30 +5,40 @@ import { ReactComponent as PinIcon } from "../../components/asset/Icon/location.
 const UnconfCard = ({ UnconfData }) => {
   const cardList = UnconfData.map((item, index) => (
     <Fragment key={index}>
-      <div className={classes.share}>
-        <span>{item.share}</span>
-        <span>分享你的想法</span>
-      </div>
-      <section className={classes.container}>
-        <p>{item.start}</p>
-        <div className={classes.card}>
-          <div className={classes.text}>
-            <span>{item.title}</span>
-            {item.tab.map((tab, tabIndex) => (
-              <span key={tabIndex} className={classes.tab}>
-                {tab}
-              </span>
-            ))}
-          </div>
-          <div className={classes.content}>
-            <span>{item.name}</span>
-            <span className={classes.icon}>
-              <PinIcon />
-              <span>{item.location}</span>
-            </span>
-          </div>
+      <div
+        className={`${
+          item.share === "15:00 - 15:30" ? classes["card-reverse"] : ""
+        } `}
+      >
+        <div
+          className={`${
+            item.share === "15:00 - 15:30" ? classes["share-border"] : ""
+          } ${classes.share}`}
+        >
+          <span>{item.share}</span>
+          <span>分享你的想法</span>
         </div>
-      </section>
+        <section className={classes.container}>
+          <p>{item.start}</p>
+          <div className={classes.card}>
+            <div className={classes.text}>
+              <span>{item.title}</span>
+              {item.tab.map((tab, tabIndex) => (
+                <span key={tabIndex} className={classes.tab}>
+                  {tab}
+                </span>
+              ))}
+            </div>
+            <div className={classes.content}>
+              <span>{item.name}</span>
+              <span className={classes.icon}>
+                <PinIcon />
+                <span>{item.location}</span>
+              </span>
+            </div>
+          </div>
+        </section>
+      </div>
     </Fragment>
   ));
 
